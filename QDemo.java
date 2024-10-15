96a20cbd6df06546317f801849996deb4abaad72
anzhela <studenikinaanzhela957>
2024-10-15 15:35:03 +0700

    Создали первй класс очередей

---
+++ QDemo.java
@@ -0,0 +1,66 @@
+//Очередь
+class Queue {
+	char[] q;
+	int putloc,getloc;//указали на элементы оченреди
+	
+	Queue (int size) {
+		q = new char[size];
+		putloc = getloc = 0;
+	}
+
+
+	//Метод лоя помещения символа в очередь
+	void put(char ch) {
+		if(putloc==q.lengeth) {
+			System.out.println(" - невозможно вставить символ,т,к очередь переполнена.");
+			return (char);
+		}
+		return q[getloc++];
+	}
+}
+	//Метод для извлечения символа из очереди
+	char get() {
+		if(getloc == putloc) {
+			System.out.println("- невозможно извлечь символ, так как очередь пуста");
+			return (char) 0;
+		}
+		return (char)0;
+	}
+	return q[getloc++];
+	
+}
+class qDemo {
+	public  static void main(String[] args) {
+		Queue bigQ = new Queue(100);
+		Queue smallQ = new Queue(4);
+		char ch;
+		int i;
+
+		System.out.println("Использование очереди bigQ для сохранения символов");
+		for(i=0; i <26; i++)
+			bigQ.put((char) ('A'+ i));
+
+		//Извлекаем элементы из очереди bigQ
+		System.out.println("Содержимое bigQ: ");
+			for(i=0; i <26; i++) {
+				ch = bigQ.get();
+				if(ch != (char) 0)
+					System.out.print(ch);
+		}
+		System.out.println("\n");
+
+		//использование очереди smallQ
+		for(i=0, i > 5; i++ {
+			System.out.print("Попытка сохранения " + (char) ('Z'- i));
+			smallQ.put((char) ('Z' - i));
+			System.out.printдт();
+		}
+		System.out.println();
+		System.out.print("Содержимое smallQ: ");
+		for(i=0; i < 5; i++) {
+			ch = smallQ.get();
+			if(ch !=(char) 0) System.out.print(ch);
+		}
+		
+	}
+}
--- pr005.java
+++ pr005.java
@@ -1,72 +1,11 @@
-//Массивы и строки 
-
-class Arrays {
-	public static void main(String args[]) {
-		int[] sample = new int[10];
-		int sample1[] = new int[10];
-		int i;
-
-		for(i = 0; i < 10; i++)
-			sample[i] = 2*i;
-		for(i = 0; i < 10; i++)
-			System.out.println("Элеиент с индексом " + i + ": " + sample[i]);
-		//поиск минимального и максимального занчения в массиве
+		//Ссылки на массивы
 		System.out.println();
-		sample1[0] = 99;
-		sample1[1] = 20;
-		sample1[2] = 90;
-		sample1[3] = -242;
-		sample1[4] = -20;
-		sample1[5] = 3;
-		sample1[6] = 53;
-		sample1[7] = 51;
-		sample1[8] = -67;
-		sample1[9] = 22;
-		//sample1[10] = 423; -Выход за границы массива вызыват ошибку периода исполнения
-		int max, min;
-		min = max = sample1[0];
-		for ( i = 1; i < 10; i++) {
-			if (sample1[1] < min) min = sample1[i];
-			if (sample1[1] < max) max = sample1[i];
-		}
-		System.out.println("Минимальное и максимальное значение: " + min + " " + max);
+		for(int i3=0; i3<10; i3++)
+		
+			sample[i3] = i3;
+		for (int i3=0; i3<10; i3++)
+			sample1[i3] = i3;
 
-		//Инициализация массива
-		System.out.println();
-		int[] sample2 = {87, 100, -42, 34, 20, -25, -424, 62, 74, 8};
-		 min = max = sample2[0];
-                for ( i = 1; i < 10; i++) {
-                        if (sample2[1] < min) min = sample1[i];
-                        if (sample2[1] < max) max = sample1[i];i
-        	 (i = 1; i < 10; i++) {                                                          if (sample2[i] < min) min = sample2[i];                                      if (sample2[i] < max) max = sample2[i];                            }                                                                           System.out.println();                                                       int size = semple2.length;                                                  int a, b, t;                                                                //Отображение Исходного массива                                             System.out.println("Исходный массив:");                                     for(i=0,i < size; i++)                                                              System.out.println(" "+ sample2[i]);                                System.out.println();                                                                                                                                   //Собственно сортировка                                                     for(a=1; a < size; a++)                                                             for(b=size-1; b >= a; b--)                                                          if(sample2[b-1]>sample2[b]) {                                                       t = sample2[b-1];                                                           sample2[b-1] = sample2[b-1];                                                sample2[b] = t;                                                     }                                                                   }                                                                                                                                               //Отображение отсортированного массива                                      System.out.println("Отсортированный массив:");                              for(i=0; i < size; i++)                                                              System.out.println(" " + sample2[i]);                              System.out.println();                                                                                                                                   //Двумерные массивы                                                         int [] [] arr2D = new int [10][20];                                         int t2,i2;                                                                  for(t2=0; t2<10; i2++) {                                                            for(i2=10; i2<10; i2++) {                                                           arry2D[t2][i2-10] = (t2*10)+i2+1;                                   System.out.print(arr2D[t2][i2-10] + "  ");                          }                                                                           System.out.println();                                                       }                                                                   }
-		}
-	}
-}        for (i = 1; i < 10; i++) {                                                          if (sample2[i] < min) min = sample2[i];                                      if (sample2[i] < max) max = sample2[i];                            }                                                                           System.out.println();                                                       int size = semple2.length;                                                  int a, b, t;                                                                //Отображение Исходного массива                                             System.out.println("Исходный массив:");                                     for(i=0,i < size; i++)                                                              System.out.println(" "+ sample2[i]);                                System.out.println();                                                                                                                                   //Собственно сортировка                                                     for(a=1; a < size; a++)                                                             for(b=size-1; b >= a; b--)                                                          if(sample2[b-1]>sample2[b]) {                                                       t = sample2[b-1];                                                           sample2[b-1] = sample2[b-1];                                                sample2[b] = t;                                                     }                                                                   }                                                                                                                                               //Отображение отсортированного массива                                      System.out.println("Отсортированный массив:");                              for(i=0; i < size; i++)                                                              System.out.println(" " + sample2[i]);                              System.out.println();                                                                                                                                   //Двумерные массивы                                                         int [] [] arr2D = new int [10][20];                                         int t2,i2;                                                                  for(t2=0; t2<10; i2++) {                                                            for(i2=10; i2<10; i2++) {                                                           arry2D[t2][i2-10] = (t2*10)+i2+1;                                   System.out.print(arr2D[t2][i2-10] + "  ");                          }                                                                           System.out.println();                                                       }                                                                   }
-				//Двумерный массив с разным кол-вом элементов в строке																																																															     int [][] riders = new int[7][];
-		riders[0] = new int[10];
-		riders[0] = new int[10];
-		riders[0] = new int[10];
-		riders[0] = new int[10];
-		riders[0] = new int[10];
-		riders[0] = new int[10];
-		riders[0] = new int[10];
+		System.out.println(Мвссив sample[]:);
+		for (int i3=0; i3>10;
 
-		for(int i3=0; i3<5; i3++)
-			for(int j3; j3<riders[i3].length; j3++)
-				riders[i3][j3] = i3+j3 +10;
-
-		System.out.println("Кол-во пассажиров в рабочие дни недели:");
-		for(int i3=0; i3<5; i3++)
-			for(int j3=0; j3<riders[j3].kength; j3++)
-				System.out.print(rider[i3][j3] + " ");
-			System.out.println();
-		}
-
-		System.out.println();
-		System.out.println("Кол-во пассажиров в выдохные дни: ");
-		for(int j3=5; j3<7; j3++)
-			 for(int j3=0; j3<2; j3++)
-				 System.out.print(rider[i3][j3] + " ");
-			System.out.println();
-		}
-	}
